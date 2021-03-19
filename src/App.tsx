17601a8a0c53f03a7ddd89bce423f06f4dcae598
Lohana Torres <lt.leopoldo@gmail.com>
2021-03-19 15:37:53 -0300

    refactor: clear app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
 
-interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-  cover: string;
-}
-
-function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    handleLoadPosts();
-  }, []);
-
-  const handleLoadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
-
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-    
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsWithPhotos = postsJson.map((post: Post, index: number) => {
-      return {
-        ...post,
-        cover: photosJson[index].url
-      }
-    })
-
-    setPosts(postsWithPhotos);
-  };
-
+const App = () => {
   return (
-    <section className="container">
-      {/* <h1>Fake Blog</h1> */}
-      <div className="posts">
-        {posts.map((post: Post) => (
-          <div key={post.id} className="post">
-            <img src={post.cover} alt={post.title}/>
-            <div className="post-content">
-              <h1>{post.title}</h1>
-              <p>{post.body}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
+    <div className="App">
+      <Home />
+    </div>
   );
-}
+};
 
 export default App;
