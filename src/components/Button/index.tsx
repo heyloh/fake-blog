178d086bd5a9d2b2f337f3327a4685b6f160072d
Lohana Torres <lt.leopoldo@gmail.com>
2021-06-26 15:36:02 -0300

    feat: disable button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,10 +3,11 @@ import './styles.css';
 interface ButtonProps {
   action: () => void;
   text: string;
+  disabled: boolean;
 }
 
-const Button = ({ text, action }: ButtonProps) => {
-  return <button onClick={action}>{text}</button>;
+const Button = ({ text, action, disabled }: ButtonProps) => {
+  return <button onClick={action} disabled={disabled}>{text}</button>;
 };
 
 export default Button;
